4311d3bace63e9e1ac7efa2114cd3546413ec876
nschm-101297 <96783065+nschm-101297@users.noreply.github.com>
2021-12-28 15:56:04 +0100

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,7 @@ namespace ShareLoader
 
         public IConfiguration Configuration { get; }
 
+        //Ich kann das viel besser als du!
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
