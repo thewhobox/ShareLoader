48803d516994b3cfa1d8aee5e0f1154aaf9c7920
Mike <45664417+thewhobox@users.noreply.github.com>
2024-12-04 12:30:19 +0100

    Update SettingsController and SettingsModel to handle null settings and modify default folder paths

--- ShareLoader/Models/SettingsModel.cs
+++ ShareLoader/Models/SettingsModel.cs
@@ -2,10 +2,10 @@ namespace ShareLoader.Models;
 
 public class SettingsModel
 {
-    public string DownloadFolder { get; set; } = "/share";
-    public string MoveFolder { get; set; } = "/share/out";
-    public string LogFolder { get; set; } = "/share";
+    public string DownloadFolder { get; set; } = "/shareloader";
+    public string MoveFolder { get; set; } = "/emby";
+    public string LogFolder { get; set; } = "/shareloader";
     public string ZipPath { get; set; } = "7z";
     public string ZipArgs { get; set; } = "e \"%filesDir%\" -bsp1 -aoa -o\"%extractDir%\" -p%pass%";
-    public int MaxDownloads { get; set; } = 1;
+    public int MaxDownloads { get; set; } = 3;
 }
